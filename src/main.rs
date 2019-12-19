ebeb9f10a6e6dc097378494cf5e9b1a66e84283e
Anton Kuzmin <sassanid18@gmail.com>
2019-12-19 23:25:55 +0300

    correct but bad algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
-// use soup::prelude::*;
 use scraper::{ElementRef, Html, Selector};
 use selectors::attr::CaseSensitivity;
 use std::error::Error;
-// use scraper::node::Element;
+
+struct ProposalRawData {
+    title: String,
+    text_reference: String,
+}
+
+impl ProposalRawData {
+    // return vec because right now i have only vec
+    pub fn new() -> Result<Vec<ProposalRawData>, Box<dyn Error>> {
+        let mut rfcs: Vec<ProposalRawData> = Vec::new();
+
+        let html = reqwest::get("https://github.com/rust-lang/rfcs/tree/master/text")?.text()?;
+        let document = Html::parse_document(html.as_ref());
+
+        let selector_a = Selector::parse("a").unwrap();
+
+        for a in document.select(&selector_a) {
+            let a: ElementRef = a;
+
+            if a.value()
+                .has_class("js-navigation-open", CaseSensitivity::CaseSensitive)
+            {
+                if let Some(pat) = a.value().attr("href") {
+                    if pat != "" && pat != "/rust-lang/rfcs" {
+                        let title = a.value().attr("title").expect("huiinya").into();
+                        let href = a.value().attr("href").expect("what a hell").into();
+
+                        rfcs.push(ProposalRawData {
+                            title: title,
+                            text_reference: href,
+                        });
+                    }
+                }
+            }
+        }
+
+        Ok(rfcs)
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let html = reqwest::get("https://github.com/rust-lang/rfcs/tree/master/text")?.text()?;
+    let proposals = ProposalRawData::new()?;
 
-    let document = Html::parse_document(html.as_ref());
-    // let selector = Selector::parse("tbody").unwrap();
-
-    let mut rfcs_title = Vec::new();
-    let mut rfcs_href = Vec::new();
-
-    let selector_a = Selector::parse("a").unwrap();
-
-    for a in document.select(&selector_a) {
-        let a: ElementRef = a;
-
-        if a.value()
-            .has_class("js-navigation-open", CaseSensitivity::CaseSensitive)
-        {
-            println!("{:?}", a.value().attr("title"));
-            println!("{:?}", a.value().attr("href"));
-
-            
-            rfcs_title.push(a.value().attr("title"));
-            rfcs_href.push(a.value().attr("href"));
-        }
-    }
-
-    dbg!(rfcs_href.len());
-    dbg!(rfcs_title.len());
+    dbg!(&proposals[0].title);
+    dbg!(&proposals[0].text_reference);
 
     Ok(())
 }
