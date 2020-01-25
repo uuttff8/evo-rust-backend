fa1137bfef2cd5e4280a2d4a8846c751ca1bd3a7
Anton Kuzmin <sassanid18@gmail.com>
2020-01-25 16:57:23 +0300

    refactor and move to tokio-postgres

---
+++ src/db.rs
@@ -0,0 +1,70 @@
+use std::thread;
+use std::time::Duration;
+
+use crate::proposals::ProposalsRawData;
+use crate::proposals::{ProposalJson, ProposalsJson};
+
+use lazy_static::lazy_static;
+use tokio_postgres::NoTls;
+
+lazy_static! {
+    static ref DB_ADRESS: &'static str = "host=localhost user=postgres";
+}
+
+pub async fn get_proposals() -> String {
+    let (client, connection) = tokio_postgres::connect(*DB_ADRESS, NoTls).await.unwrap();
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    let mut body: String = "".into();
+    for row in client
+        .query("SELECT body FROM proposals", &[])
+        .await
+        .expect("SELECT body is not queried")
+    {
+        let value: String = row.get("body");
+        body = value;
+    }
+    body
+}
+
+pub async fn generate_proposals() {
+    thread::spawn(async move || {
+        let (client, _) = tokio_postgres::connect(*DB_ADRESS, NoTls).await.unwrap();
+        loop {
+            let props: String = process_props().unwrap().join("\n");
+            let update_db = client
+                .execute("UPDATE proposals SET body = $1 WHERE id = 1;", &[&props])
+                .await;
+            println!("{:?}", update_db);
+            thread::sleep(Duration::from_secs(900));
+        }
+    });
+}
+
+fn process_props() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let proposals = ProposalsRawData::new()?.proposal;
+
+    let mut prop_array = Vec::<String>::new();
+
+    for mut prop in proposals {
+        let prop_json = ProposalJson {
+            title: prop.title.to_string(),
+            id: prop.get_id().to_string(),
+            date: prop.get_date()?.to_string(),
+            issue: prop.get_issue_link()?.to_string(),
+        };
+
+        let props_json = ProposalsJson {
+            proposal: prop_json,
+        };
+
+        let j = serde_json::to_string(&props_json)?;
+        prop_array.push(j);
+    }
+
+    Ok(prop_array)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,11 @@
 #![feature(async_closure)]
 
+mod db;
 mod proposals;
 
-use crate::proposals::{ProposalJson, ProposalsJson};
-use crate::proposals::{ProposalRawData, ProposalsRawData};
-
-use std::thread;
-use std::time::Duration;
+use crate::proposals::ProposalRawData;
 
 use actix_web::{web, App, HttpServer, Responder};
-use lazy_static::lazy_static;
-use postgres::{Client, NoTls};
-
-lazy_static! {
-    static ref DB_ADRESS: &'static str = "host=localhost user=postgres";
-}
-
-fn process_props() -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let proposals = ProposalsRawData::new()?.proposal;
-
-    let mut prop_array = Vec::<String>::new();
-
-    for mut prop in proposals {
-        let prop_json = ProposalJson {
-            title: prop.title.to_string(),
-            id: prop.get_id().to_string(),
-            date: prop.get_date()?.to_string(),
-            issue: prop.get_issue_link()?.to_string(),
-        };
-
-        let props_json = ProposalsJson {
-            proposal: prop_json,
-        };
-
-        let j = serde_json::to_string(&props_json)?;
-        prop_array.push(j);
-    }
-
-    Ok(prop_array)
-}
-
-async fn generate_proposals() {
-    thread::spawn(async move || {
-        let mut client = Client::connect(*DB_ADRESS, NoTls).unwrap();
-
-        loop {
-            let props: String = process_props().unwrap().join("\n");
-            let update_db =
-                client.execute("UPDATE proposals SET body = $1 WHERE id = 1;", &[&props]);
-            println!("{:?}", update_db);
-
-            thread::sleep(Duration::from_secs(900));
-        }
-    });
-}
 
 async fn get_proposal(info: web::Path<String>) -> impl Responder {
     let props = ProposalRawData::new(info.into_inner()).unwrap();
@@ -61,23 +13,7 @@ async fn get_proposal(info: web::Path<String>) -> impl Responder {
 }
 
 async fn get_proposals() -> impl Responder {
-    let thread: String = thread::spawn(move || {
-        let mut client = Client::connect(*DB_ADRESS, NoTls).unwrap();
-        let mut body: String = "".into();
-        for row in client
-            .query("SELECT body FROM proposals", &[])
-            .expect("SELECT body is not queried")
-        {
-            let value: String = row.get("body");
-            body = value;
-        }
-
-        body
-    })
-    .join()
-    .unwrap();
-
-    thread
+    db::get_proposals().await
 }
 
 async fn index() -> impl Responder {
@@ -86,7 +22,7 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    generate_proposals().await;
+    db::generate_proposals().await;
 
     HttpServer::new(|| {
         App::new()
